149bd00042690ba30fa5e38acca5883bb382a150
Shaheda Hafid <abdelhafid.idbahamd@gmail.com>
2026-03-30 19:01:52 +0100

    Adding empty field input registration test

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -10,10 +10,10 @@ public class RegisterPage extends BasePage {
     private By firstNameInput = By.cssSelector("input#first_name");
     private By lastNameInput = By.cssSelector("input#last_name");
     private By emailFieldInput = By.cssSelector("input#email");
-     private By passwordInput = By.cssSelector("input#password");
+    private By passwordInput = By.cssSelector("input#password");
     private By registerButton = By.className("action_bottom");
     private By registerHeader = By.className("accounts-title");
-
+    private By errorMessage = By.cssSelector("#create-account .errors li");
 
     public RegisterPage(WebDriver driver){
         super(driver);
@@ -39,5 +39,7 @@ public class RegisterPage extends BasePage {
     public String checkRegisterHeader(){
         return getValue(registerHeader);
     }
-
+    public String checkErrorMessage(){
+        return getValue(errorMessage);
+    }
 }
